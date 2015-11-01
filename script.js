07656d73a081f7e7fbfe4c1eb5b1b8f6561494fa
Thomas <thomas@onlineth.com>
2015-10-31 21:29:40 -0500

    Everything

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+$('#Done').hide(0);
+$('#alertBox').hide(0);
+$('#extraBreaks').hide(0);
+$('#GO').click(function() {
+    startAll();
+});
+$('#youtubeInputURL').keypress(function(e) {
+    if (e.which == 13) {
+        startAll();
+    }
+});
+
+function startAll() {
+    $('#GO').addClass('active');
+    var url = $('#youtubeInputURL').val();
+    var id = url.substring(url.indexOf('?') + 3);
+    if (id.indexOf('&') !== -1) {
+        id = id.substring(0, id.indexOf('&'));
+    }
+    YoutubeVideo(id);
+}
+var parseQueryString = function(queryString) {
+    var params = {},
+        queries, temp, i, l;
+    // Split into key/value pairs
+    queries = queryString.split("&");
+    // Convert the array of strings into an object
+    for (i = 0, l = queries.length; i < l; i++) {
+        temp = queries[i].split('=');
+        params[temp[0]] = temp[1];
+    }
+    return params;
+};
+new Clipboard('.btn');
+
 function getWebsite(url) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, false);
@@ -33,31 +68,44 @@ function decodeStreamMap(url_encoded_fmt_stream_map) {
     }
     return sources;
 }
+
 function YoutubeVideo(youtubeId) {
-var videoInfo = getWebsite('http://jsonp.afeld.me/?url=http://www.youtube.com/get_video_info?video_id='+youtubeId);
-if (videoInfo === '') {
-    //Error
-}
-var video = decodeQueryString(videoInfo);
-if (video.status === "fail") {
-    //Error
-}
-var videoSources = decodeStreamMap(video.url_encoded_fmt_stream_map);
-
-
+    var videoInfo = getWebsite('http://jsonp.afeld.me/?url=http://www.youtube.com/get_video_info?video_id=' + youtubeId);
+    if (videoInfo === '') {
+        reportError('There was an issue download the video info');
+    }
+    var video = decodeQueryString(videoInfo);
+    if (video.status === "fail") {
+        reportError('There was an issue processing you video url');
+    }
+    var videoSources = decodeStreamMap(video.url_encoded_fmt_stream_map);
     var key, source, _ref;
     _ref = videoSources;
     var VideoURL = [{}];
     for (key in _ref) {
         source = _ref[key];
         if (source.type.match('video/mp4')) {
-        	VideoURL.push(source);
+            VideoURL.push(source);
         }
     }
-    var mainVideo = {};
-    mainVideo.URL = VideoURL;
-    mainVideo.author = video.author;
-    mainVideo.image = video.iurl;
-    mainVideo.title = (video.title).replace(/\+/g, " ");
-    return mainVideo;
+    $('#alertBox').hide('400');
+    $('#focusedInput').val(VideoURL[1].url);
+    $('#clipboard').attr('data-clipboard-text', VideoURL[1].url);
+    $("#downloadSubButton").attr("href", VideoURL[1].url);
+    //$("#downloadSubButton").attr("download", VideoURL[1].url);
+    $('#GO').removeClass('active');
+    $('#Done').show('250', function() {
+        $('#Input').hide('250');
+    });
+    $('#focusedInput').focus();
+}
+$('#GoBack').click(function() {
+    $('#Input').show('500');
+    $('#Done').hide('500');
+});
+
+function reportError(message) {
+    $('#GO').removeClass('active');
+    $('#alertBox').show('400');
+    $('#alertInnerBox').html("<div class=\"alert alert-dismissible alert-danger\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button><strong>Oh snap!</strong><br>" + message + "</div>");
 }
